4e4a44be071de3641ea269ad2e9497c49892ed00
bc-andreadao <daoam22@gmail.com>
2026-02-20 15:55:29 -0800

    update sample for BC4WP title

--- src/sampleData.ts
+++ src/sampleData.ts
@@ -31,6 +31,14 @@ const samples: sampleData[] = [
             "about": "",
             "type": "Article"
         },
+        {
+            "title": "BigCommerce for Wordpress (BC4WP) Documentation Update",
+            "description": "Learn how I streamlined a workflow to increase external contributions to the open-source BC4WP documentation",
+            "url": "bc4wp-documentation-update",
+            "sampleURL": "https://developer.bigcommerce.com/resource-hub/bc4wp-documentation-update",
+            "about": "This article explains how I automated a workflow for updating BigCommerce for WordPress (BC4WP) reference documentation. I piloted the workflow so that reference documentation could be maintained at large scale while supporting external contributions.\n\nFirst, I added DocBlocks in the BC4WP source code for its classes, methods, and hooks. Then, I created a GitHub Action that parses source-code DocBlocks and outputs the reference documentation to Markdown files. The workflow runs a shell script that uses Docker to run a phpDocumentor. I then rendered the Markdown with a Nextra site and deploy the site on Vercel. External contributors can manually trigger the workflow to update the documentation when they update DocBlock descriptions.\n\nThe sample shows I have experience working with the following:\n\n- Docs-as-code workflows\n- GitHub actions, CI/CD, automations\n- Markdown, static site generators (Nextra)\n- Writing DocBlocks & reading production code\n- Maintaining large docs at scale\n- Process and editorial improvements\n- Contribution workflows, review/rollback, and preview environments\n- AI tools to facilitate documentatation (writing DocBlocks, creating workflows)",
+            "type": "Article"
+        },
     ]
 
 export default samples;
